e60abcdabf6e01b2ff1bf97d1e6e756b09b306c1
gabrielrosin <gabriel.s.rosin@gmail.com>
2022-09-06 08:48:30 -0300

    Adds useForm hook to handle forms

--- src/Login/SignIn.js
+++ src/Login/SignIn.js
@@ -1,16 +1,45 @@
 import { LoginWrapper } from "./style";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "../Shared/useForm";
 
 export default function SignIn() {
+    const [form, handleForm] = useForm({
+        initState: {
+            email: "",
+            password: "",
+        }
+    });
     const navigate = useNavigate();
+    function sendForm(e) {
+        e.preventDefault();
+        console.log(form);
+    }
 
     return (
         <LoginWrapper>
             <h1>MyWallet</h1>
-            <form>
-                <input type="email" placeholder="E-mail" required></input>
-                <input type="password" placeholder="Senha" required></input>
-                <input type="submit" value="Entrar" required></input>
+            <form onSubmit={sendForm}>
+                <input
+                    type="email"
+                    placeholder="E-mail"
+                    name="email"
+                    value={form.email}
+                    onChange={handleForm}
+                    required
+                ></input>
+                <input
+                    type="password"
+                    placeholder="Senha"
+                    name="password"
+                    value={form.password}
+                    onChange={handleForm}
+                    required
+                ></input>
+                <input
+                    type="submit"
+                    value="Entrar"
+                    required
+                ></input>
             </form>
             <p onClick={() => navigate('/sign-up')}>Primeira vez? Cadastre-se!</p>
         </LoginWrapper>
--- src/Login/SingUp.js
+++ src/Login/SingUp.js
@@ -1,18 +1,66 @@
 import { useNavigate } from "react-router-dom";
+import { useForm } from "../Shared/useForm";
 import { LoginWrapper } from "./style";
 
 export default function SignUp() {
+    const [form, handleForm] = useForm({
+        initState: {
+            name: "",
+            email: "",
+            password: "",
+            passwordConfirmation: "",
+        }
+    }
+    )
     const navigate = useNavigate();
+
+    function sendForm(e) {
+        e.preventDefault();
+        console.log(form);
+        navigate('/');
+    }
 
     return (
         <LoginWrapper>
             <h1>MyWallet</h1>
-            <form>
-                <input type="text" placeholder="Nome" required></input>
-                <input type="email" placeholder="E-mail" required></input>
-                <input type="password" placeholder="Senha" required></input>
-                <input type="password" placeholder="Confirme a senha" required></input>
-                <input type="submit" value="Cadastrar" required></input>
+            <form onSubmit={sendForm}>
+                <input
+                    type="text"
+                    placeholder="Nome"
+                    name="name"
+                    value={form.name}
+                    onChange={handleForm}
+                    required
+                ></input>
+                <input
+                    type="email"
+                    placeholder="E-mail"
+                    name="email"
+                    value={form.email}
+                    onChange={handleForm}
+                    required
+                ></input>
+                <input
+                    type="password"
+                    placeholder="Senha"
+                    name="password"
+                    value={form.password}
+                    onChange={handleForm}
+                    required
+                ></input>
+                <input
+                    type="password"
+                    placeholder="Confirme a senha"
+                    name="passwordConfirmation"
+                    value={form.passwordConfirmation}
+                    onChange={handleForm}
+                    required
+                ></input>
+                <input 
+                    type="submit"
+                    value="Cadastrar" 
+                    required
+                ></input>
             </form>
             <p onClick={() => navigate('/')}>Já tem uma conta? Entre agora!</p>
         </LoginWrapper>
