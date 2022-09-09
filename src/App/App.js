9cf030b3aca0cdbe009463f34d4e539ef9b0ec20
gabrielrosin <gabriel.s.rosin@gmail.com>
2022-09-08 22:19:46 -0300

    Fixes a typo

--- src/App/App.js
+++ src/App/App.js
@@ -2,13 +2,14 @@ import GlobalStyle from "../Shared/globalStyle";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignIn from "../Components/Login/SignIn";
 import { AppWrapper } from "../Shared/styles";
-import SignUp from "../Components/Login/SingUp";
+import SignUp from "../Components/Login/SignUp";
 import Extract from "../Components/Extract/Extract";
 import TransactionForm from "../Components/Transaction/TransactionForm";
 import { useState } from "react";
 
 export default function App() {
   const [token, setToken] = useState('');
+  console.log(token);
 
   return (
     <>
