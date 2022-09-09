77a371364c994404f2b5ef93cd1a5b7208307e8f
gabrielrosin <gabriel.s.rosin@gmail.com>
2022-09-08 22:12:10 -0300

    Inserts state for the token to be passed to other apps

--- src/App/App.js
+++ src/App/App.js
@@ -5,18 +5,21 @@ import { AppWrapper } from "../Shared/styles";
 import SignUp from "../Components/Login/SingUp";
 import Extract from "../Components/Extract/Extract";
 import TransactionForm from "../Components/Transaction/TransactionForm";
+import { useState } from "react";
 
 export default function App() {
+  const [token, setToken] = useState('');
+
   return (
     <>
       <GlobalStyle />
       <AppWrapper>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<SignIn />} />
+            <Route path="/" element={<SignIn setToken={setToken} />} />
             <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/extract" element={<Extract />} />
-            <Route path="/add-transaction" element={<TransactionForm />} />
+            <Route path="/extract" element={<Extract token={token} />} />
+            <Route path="/add-transaction" element={<TransactionForm token={token} />} />
           </Routes>
         </BrowserRouter>
       </AppWrapper>
