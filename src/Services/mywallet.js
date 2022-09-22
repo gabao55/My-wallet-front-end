6bc726d585d6ba87673513fdd7dc7aa83fdec3c0
gabao55 <gabriel.s.rosin@gmail.com>
2022-09-22 18:18:35 -0300

    Sets app to deploy

--- src/Services/mywallet.js
+++ src/Services/mywallet.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiPath = 'http://localhost:5000';
+const apiPath = 'https://gabao55-my-wallet.herokuapp.com/';
 
 function registerUser(body) {
     return axios.post(`${apiPath}/auth/sign-up`, body)
